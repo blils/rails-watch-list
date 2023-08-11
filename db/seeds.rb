6ee9022946cc7d4f3d61d3bf96758a101069bd5f
Lili Banks <lili88banks@icloud.com>
2023-08-11 11:18:19 +0900

    opgosgjw

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@
 # Examples:
 #
 List.destroy_all
-Movie.destroy_all
+
 # require 'faker'
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-15.times do
+30.times do
   Movie.create( title: Faker::Movie.title, overview: Faker::TvShows::BojackHorseman.quote )
 end
 
